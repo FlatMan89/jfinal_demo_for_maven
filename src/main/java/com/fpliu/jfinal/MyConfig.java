0f7efbc66e9bb8eb5f5e8b23d29aed20ca165be6
leleliu008 <leleliu008@leleliu008-Ubuntu>
2015-07-24 02:03:13 +0800

    添加数据库的支持

--- src/main/java/com/fpliu/jfinal/MyConfig.java
+++ src/main/java/com/fpliu/jfinal/MyConfig.java
@@ -7,6 +7,8 @@ import com.jfinal.config.Interceptors;
 import com.jfinal.config.JFinalConfig;
 import com.jfinal.config.Plugins;
 import com.jfinal.config.Routes;
+import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
+import com.jfinal.plugin.c3p0.C3p0Plugin;
 
 public class MyConfig extends JFinalConfig {
 
@@ -27,7 +29,32 @@ public class MyConfig extends JFinalConfig {
 
 	@Override
 	public void configPlugin(Plugins plugins) {
+		loadPropertyFile("db.properties");
+		String dbType = getProperty("db.type");
+		
+		String jdbcUrl = getProperty(dbType + ".jdbcUrl");
+		String user = getProperty(dbType + ".user");
+		String password = getProperty(dbType + ".password");
+		String driverClass = getProperty(dbType + ".driverClass");
+		
+		System.out.println(dbType);
+		System.out.println(jdbcUrl);
+		System.out.println(user);
+		System.out.println(password);
+		System.out.println(driverClass);
 
+		// 配置C3p0数据库连接池插件
+		C3p0Plugin c3p0Plugin = new C3p0Plugin(jdbcUrl, user, password, driverClass);
+		plugins.add(c3p0Plugin);
+
+		// 配置ActiveRecord插件
+		ActiveRecordPlugin arp = new ActiveRecordPlugin(c3p0Plugin);
+		plugins.add(arp);
+		
+		boolean isDevMode = true;
+		if (isDevMode) {
+			arp.setShowSql(true);
+		}
 	}
 
 	@Override
